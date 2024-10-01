a1c421e0a654ade8b9da74807ace4efb63eabf9a
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-01 09:11:44 +0200

    task 1 with elist for task 0.

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,11 @@
-export default function getResponseFromAPI(){
-	const prom = new Promise((resolve, reject) => {
-		let success = true;
-		if (success === true){
-			resolve(true)
-		}
-		else{
-			reject(false)
-		}
-	})
-	return prom
+export default function getResponseFromAPI() {
+  const prom = new Promise((resolve, reject) => {
+    const success = true;
+    if (success === true) {
+      resolve(true);
+    } else {
+      reject(new Error('failed.'));
+    }
+  });
+  return prom;
 }
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+export default function getFullResponseFromAPI(success) {
+  const p = new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Sucess',
+      });
+    } else {
+      // eslint is picky about the returning value of reject, it must be an object
+      // of Error
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return p;
+}
