71b12e71fc107eb36231a4ecc2421fe692f6d4c7
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-01 10:45:15 +0200

    task 7

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,5 @@
+export default function loadBalancer(chinaDownload, USDownload) {
+  // the function returns the value returned by the promise that resolved the first.
+  return Promise.race([chinaDownload, USDownload])
+    .then((value) => value);
+}
