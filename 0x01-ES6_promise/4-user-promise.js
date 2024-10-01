29f2a1ecfe3e3cd92cc5ef6e49a7e9064bcd4420
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-01 10:14:37 +0200

    task 6.

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,6 @@
 export default function signUpUser(firstName, lastName) {
-  const p = new Promise((resolve) => resolve({
+  return new Promise((resolve) => resolve({
     firstName,
     lastName,
   }));
-  return p;
 }
--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,3 @@
 export default function uploadPhoto(filename) {
-  const p = new Promise((resolve, reject) => reject(new Error(`${filename} cannot be processed`)));
-  return p;
+  return new Promise((resolve, reject) => reject(new Error(`${filename} cannot be processed`)));
 }
