8131cd5d35a236372a8586de3dc344b8686c80f2
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-01 09:21:08 +0200

    task 2.

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  // we already get a promise, we just need to handle it
+  return promise.then(() => ({
+    status: 200,
+    body: 'Sucess',
+  }))
+    .catch(() => new Error())
+    .finally(console.log('Got a response from the API'));
+}
