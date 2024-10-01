6d34fa51da93883e7e35f87f4ebcf1d56ffd804f
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-01 18:42:06 +0200

    fixing task 2.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   // we already get a promise, we just need to handle it
   return promise.then(() => ({
     status: 200,
-    body: 'Sucess',
+    body: 'Success',
   }))
     .catch(() => new Error())
     .finally(console.log('Got a response from the API'));
