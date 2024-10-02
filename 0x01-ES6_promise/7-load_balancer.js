aaae88b50608d4a8bb985fba8375bf31cbcb969b
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-02 09:31:08 +0200

    Transforming task 7 into an async function.

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
-export default function loadBalancer(chinaDownload, USDownload) {
+export default async function loadBalancer(chinaDownload, USDownload) {
   // the function returns the value returned by the promise that resolved the first.
   return Promise.race([chinaDownload, USDownload])
     .then((value) => value);
