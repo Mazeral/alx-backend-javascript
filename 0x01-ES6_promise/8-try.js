81f105c6cf6254fe69f9825e7604248ee789130d
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-02 09:39:23 +0200

    Fixing task 8.

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   // You don't need try block to throw an error.
-  if (denominator === 0) throw new Error('cannot divide by -1');
+  if (denominator === 0) throw Error('cannot divide by -1');
   else {
     return numerator / denominator;
   }
