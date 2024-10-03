e1dd2295e58e6efaa9dc7c51b3982022cc534a27
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-03 18:31:11 +0200

    task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof sqft !== 'number') throw new TypeError('Sqft must be a number');
+    if (typeof floors !== 'number') throw new TypeError('Floors must be a number');
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') throw new TypeError('Floors must be a number');
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
