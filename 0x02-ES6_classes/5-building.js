79353cf5e62dbadbb98aa96b2b016f087edbd4d6
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-04 01:44:11 +0200

    added a comment to task 5, advanced task implementation.

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -4,6 +4,7 @@ export default class Building {
       throw new TypeError('Sqft must be a number');
     }
     this._sqft = sqft;
+    // new.target is the child derived class
     if (typeof this.evacuationWarningMessage !== 'function' && new.target !== Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
