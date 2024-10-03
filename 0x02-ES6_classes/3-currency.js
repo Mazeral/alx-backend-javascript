5c0517a64e565c7a010105e962ebffff31e78e6e
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-03 14:50:20 +0200

    task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') throw new TypeError('Code must be a string');
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') throw new TypeError('Code must be a string');
+    this._code = value;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    console.log(`${this._code} ${this._name}`);
+  }
+}
