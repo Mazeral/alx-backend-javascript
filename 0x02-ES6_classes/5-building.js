c50f483a5a5366c9434153e5344e082c053fe4cf
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-03 18:21:26 +0200

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,12 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
     if (typeof sqft !== 'number') {
       throw new TypeError('Sqft must be a number');
     }
     this._sqft = sqft;
+    if (typeof this.evacuationWarningMessage !== 'function' && new.target !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
