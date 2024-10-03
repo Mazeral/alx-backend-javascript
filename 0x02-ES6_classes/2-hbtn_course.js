0e696aab8b83f0e39d4ac46829e71b84242c2cea
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-03 14:35:08 +0200

    Uncommented the type checking lines.

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,19 +1,19 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    // if (typeof name !== 'string') {
-    //   throw new TypeError('Name must be a string');
-    // }
-    // if (typeof length !== 'number') {
-    //   throw new TypeError('Length must be a number');
-    // }
-    // if (typeof students !== 'object') {
-    //   throw new TypeError('Students must be an array');
-    // }
-    // for (const name of students) {
-    //   if (typeof name !== 'string') {
-    //     throw new TypeError('Students must be an array of strings');
-    //   }
-    // }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (typeof students !== 'object') {
+      throw new TypeError('Students must be an array');
+    }
+    for (const name of students) {
+      if (typeof name !== 'string') {
+        throw new TypeError('Students must be an array of strings');
+      }
+    }
     this._name = name;
     this._length = length;
     this._students = students;
