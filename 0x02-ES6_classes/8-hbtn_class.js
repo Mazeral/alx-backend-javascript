7240b935e0d039223eee2a8e75e64d6d8e3aaf64
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-03 20:28:52 +0200

    eslinting task 8

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,32 +1,34 @@
-export default class HolbertonClass{
-	constructor (size, location){
-		if (typeof size !== 'number') throw new TypeError("Size must be a number")
-		if (typeof location !== 'string') throw new TypeError("Location must be a string")
-		this._size = size;
-		this._location = location;
-	}
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') throw new TypeError('Size must be a number');
+    if (typeof location !== 'string') throw new TypeError('Location must be a string');
+    this._size = size;
+    this._location = location;
+  }
 
-	get size () {
-		return this._size	
-	}
-	get location () {
-		return this._location	
-	}
+  get size() {
+    return this._size;
+  }
 
-	set size (value) {
-		if (typeof value !== 'number') throw new TypeError("Size must be a number")
-		this._size = value
-	}
-	set location (value) {
-		
-		if (typeof value !== 'string') throw new TypeError("Location must be a string")
-		this._location = value
-	}
+  get location() {
+    return this._location;
+  }
 
-	valueOf(){
-		return this._size
-	}
-	toString(){
-		return this._location
-	}
+  set size(value) {
+    if (typeof value !== 'number') throw new TypeError('Size must be a number');
+    this._size = value;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') throw new TypeError('Location must be a string');
+    this._location = value;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
 }
