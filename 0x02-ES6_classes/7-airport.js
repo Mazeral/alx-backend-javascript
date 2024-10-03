461eb28fdc92190d229443dcb6187bd0b5268a7f
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-03 18:41:33 +0200

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof code !== 'string') throw new TypeError('Code must be a string');
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    this._name = value;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') throw new TypeError('Code must be a string');
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
