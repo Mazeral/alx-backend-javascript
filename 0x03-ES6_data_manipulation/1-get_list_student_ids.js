64835b88811ce7e082b4faba4ba5b135b25e70ab
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-07 13:26:37 +0200

    task 0

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,13 @@
+export default function getListStudentIds(arrOfObj){
+	console.log("Hello!")
+	// console.log(Array.isArray(arrOfObj))
+	// if (Array.isArray(arrOfObj) === true){
+	// 	idArr = []
+	// 	arrOfObj.map((value)=>{
+	// 		console.log(value)
+	// 		idArr.push(value["id"])
+	// 	})
+	// 	return idArr
+	// }
+	// else return []
+}
