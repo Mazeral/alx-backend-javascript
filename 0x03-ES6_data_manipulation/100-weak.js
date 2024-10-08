75045d368d49a3ec72983b059e915e09edf72f85
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-08 12:11:43 +0200

    Fixing advanced.

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,14 +3,14 @@ export const weakMap = new WeakMap();
 
 // 2. Export the queryAPI function
 export function queryAPI(endpoint) {
-  // Check if the endpoint already exists in the weakMap, if not set it to 0
-  const count = weakMap.get(endpoint) || 0;
-
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
   // If the count is greater than or equal to 5, throw an error
-  if (count >= 5) {
+  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
 
   // Increment the count and set it back to the weakMap
-  weakMap.set(endpoint, count + 1);
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 }
