c123b5d6f6172725275aeba2853d20a511e82576
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-08 12:13:08 +0200

    Fixing advanced.

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -6,11 +6,11 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
+  // Increment the count and set it back to the weakMap
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   // If the count is greater than or equal to 5, throw an error
   if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
 
-  // Increment the count and set it back to the weakMap
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 }
