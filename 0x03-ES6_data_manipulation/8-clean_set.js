8fedac674c339f030fe5974df3cbb54cc065d752
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-08 11:56:39 +0200

    Fixing task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,16 @@
 export default function cleanSet(set, startString) {
-  let string = '';
-  const array = Array.from(set);
-  if (startString === '') return '';
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i].includes(startString)) {
-      string += array[i].replace(startString, '');
-      if (i + 1 < array.length) string += '-';
-    }
+  if (!startString || typeof startString !== 'string') {
+	return '';
   }
-  if (string.endsWith('-')) string = string.slice(0, -1);
-  return string;
+
+  let result = '';
+  
+  set.forEach((value) => {
+	if (value.startsWith(startString)) {
+	  result += `${value.slice(startString.length)}-`;
+	}
+  });
+  
+  // Remove the last hyphen if it exists
+  return result.endsWith('-') ? result.slice(0, -1) : result;
 }
