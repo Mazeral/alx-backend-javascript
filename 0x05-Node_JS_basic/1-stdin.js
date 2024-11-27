20cb63bcfe81cc2c4c9c27452ae5db4e680c6a6a
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-27 06:53:22 +0200

    task 2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
 function userInput() {
   process.stdout.write("Welcome to Holberton School, what is your name?\n");
-  process.stdin.on("data", function(data) {
-    const input = data.toString().trim();
-    process.stdout.write(`Your name is: ${input}\n`);
+  process.stdin.on("data", function(input) {
+    const text = input.toString().trim();
+    process.stdout.write(`Your name is: ${text}\n`);
     process.exit();
   });
 }
