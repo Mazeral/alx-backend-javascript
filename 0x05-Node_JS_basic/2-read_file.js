52715824602d82f8e1179929b5aae2985271e9d3
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-27 09:57:06 +0200

    trying to solve task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,16 +1,46 @@
-const fs = require('fs')
+const fs = require('fs');
 
-
-function countStudents(){
-	let results = null
-	try {
-		fs.readFile('database.csv', function(err, data){
-			results = data.toString().split(',')
-			process.stdout.write(`Number of students: ${results.length}\n`)
-		})
-	} catch (error) {
-		
-	}
+function countStudents() {
+  try {
+    let students = null;
+    let rows = null;
+    let results = null;
+    const fields = [];
+    fs.readFile('database.csv', (err, data) => {
+      results = data.toString();
+      rows = results.split('\n');
+      students = rows.filter((row) => row !== '').map((row) => row.split(','));
+      process.stdout.write(`Number of students: ${students.length - 1}\n`);
+      // Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
+      students.forEach((student) => {
+        const field = student[3];
+        if (field !== 'field' && fields.indexOf(field) === -1) {
+          fields.push(field);
+        }
+      });
+      fields.forEach((field) => {
+        let studnetsNumber = 0;
+        let studentNames = '';
+        students.forEach((student) => {
+          if (student[3] === field) {
+            studnetsNumber += 1;
+          }
+          if (!studentNames.includes(field[1])) {
+            if (students.indexOf(student) !== students.length - 1) {
+              if (student[0] !== 'firstname') {
+                studentNames += `${student[0]}, `;
+              } else {
+                studentNames += `${student[0]}`;
+              }
+            }
+          }
+        });
+        process.stdout.write(`Number of students in ${field}: ${studnetsNumber} ${studentNames}\n`);
+      });
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-module.exports = countStudents()
+module.exports = countStudents();
